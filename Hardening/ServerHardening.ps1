47887004e3175df1c84cabc4cc6207e7b40afce5
Mark Gray (MSFT) <markgray@microsoft.com>
2017-02-01 13:06:14 -0800

    Added comments

--- Hardening/ServerHardening.ps1
+++ Hardening/ServerHardening.ps1
@@ -4,6 +4,7 @@ Configuration ServerHardening
 
     Node "Server1"
     {
+        # Make sure that the Guest and DefaultAccount are disabled.
         User Guest
         {
             Ensure = 'Present'
@@ -18,6 +19,28 @@ Configuration ServerHardening
             Disabled = $true
         }
 
+        #User Account Control - prompt admin 
+        Registry ConsentPromptBehaviorAdmin
+        {
+            Ensure = "Present"
+            Key = "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System"
+            ValueName = "ConsentPromptBehaviorAdmin"
+            ValueType = "Dword"
+            ValueData = "5"
+        }  
+
+        #Interactive logon: Number of previous logons to cache (in case domain controller is not available)
+        Registry Numberofpreviouslogonstocache
+        {
+            Ensure = "Present"
+            Key = "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows NT\CurrentVersion\Winlogon"
+            ValueName = "CachedLogonsCount"
+            ValueType = "Dword"
+            ValueData = "2"
+        }
+
+
+        # Use the WindowsFeatureSet resource to ensure that a set Features are absent.
         WindowsFeatureSet DisallowedFeatures
         {
             Ensure = 'Absent'
@@ -29,6 +52,7 @@ Configuration ServerHardening
                         'ServerEssentialsRole')
         }
 
+        # Use the WindowsFeatureSet resource to ensure that a set features are present.
         WindowsFeatureSet RequiredFeatures
         {
             Ensure = 'Present'
@@ -39,6 +63,7 @@ Configuration ServerHardening
                         'Windows-Server-Backup')
         }
 
+        # Use the ServiceSet resource to stop and disable a set of services.
         ServiceSet DisallowedServices
         {
             Name = @(   'defragsvc',
@@ -49,6 +74,7 @@ Configuration ServerHardening
             StartupType = 'Disabled' 
         }
 
+        # Add a few environment variables that can be used by applications or other configurations.
         Environment Corp
         {
             Name = 'PropertyOf'
@@ -61,9 +87,10 @@ Configuration ServerHardening
             Value = 'CentralIT'
         }
 
+        # Use the GroupSet resource to make sure a specific user is not in any of the specified groups.
         GroupSet DomainUser
         {
-            Ensure = 'Present'
+            Ensure = 'Absent'
             GroupName = @('Administrators','Backup Operators','Remote Management Users', 'Power Users')
             MembersToExclude = 'contoso\bensmith'
         }
