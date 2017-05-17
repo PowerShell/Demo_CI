c753de57877a35c7cb4a77b57d6544a3aa2919f3
Michael Greene <michael.greene@microsoft.com>
2017-05-17 15:45:16 -0500

    add example module

---
+++ ExampleModule/0.1.0.0/ExampleModule.psm1
@@ -0,0 +1,48 @@
+<#
+.SYNOPSIS
+    Example function that returns a string, 'Hello, World'
+.EXAMPLE
+    New-Example1
+
+    Returns 'Hello, World'
+#>Function New-Example1 {
+
+    [Alias('Example1')]
+    [CmdletBinding()]
+    param()
+    Write-Output 'Hello, World'
+}
+
+<#
+.SYNOPSIS
+    Example function that accepts input and returns it as output
+.EXAMPLE
+    New-Example2 -text 'Hello, Word'
+
+    Returns 'Hello, World'
+#>Function New-Example2 {
+    [Alias('Example2')]
+    [CmdletBinding()]
+    param(
+        [string]$text
+    )
+    Write-Output $text
+}
+
+<#
+.SYNOPSIS
+    Example function that accepts two integers and adds them
+.EXAMPLE
+    New-Example3 -First 3 -Second 5
+
+    Returns 8
+#>Function New-Example3 {
+    [Alias('Example3')]
+    [CmdletBinding()]
+    param(
+        [int32]$First,
+        [int32]$Second
+    )
+    $Output = $First + $Second
+    Write-Output $Output
+}
